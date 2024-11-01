8e82d10642db5fd00bd56aaabb0c812413467def
Liberato <157623242+Liberatinho@users.noreply.github.com>
2024-11-01 19:18:32 -0300

    ArrumandoErro de log Comunidade

--- src/main/java/com/example/servlettrocatine/servlet/comunidade/InserirComunidade.java
+++ src/main/java/com/example/servlettrocatine/servlet/comunidade/InserirComunidade.java
@@ -1,7 +1,9 @@
 package com.example.servlettrocatine.servlet.comunidade;
 
 import com.example.servlettrocatine.DAO.ComunidadeDAO;
+import com.example.servlettrocatine.DAO.LogDAO;
 import com.example.servlettrocatine.model.Comunidade;
+import com.example.servlettrocatine.model.Log;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,14 +18,35 @@ import java.io.IOException;
 public class InserirComunidade extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         ComunidadeDAO comunidadeDAO = new ComunidadeDAO();
-        int id = Integer.parseInt(request.getParameter("id"));
+
         String nome = request.getParameter("nome");
         String criador = request.getParameter("criador");
         String descricao = request.getParameter("descricao");
+        String foto = request.getParameter("foto");
         int qntIntegrantes = Integer.parseInt(request.getParameter("qntIntegrantes"));
-        Comunidade comunidade = new Comunidade(id, nome, criador, descricao, qntIntegrantes, "");
 
-        comunidadeDAO.inserirComunidade(comunidade);
+        int idAdm = (Integer) request.getSession().getAttribute("idAdm");
+
+        Comunidade comunidade = new Comunidade(nome, criador, descricao, qntIntegrantes, foto);
+
+        if (nome == null || criador == null || descricao == null || foto == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Todos os campos são obrigatórios.");
+            return;
+        }
+        boolean certo = comunidadeDAO.inserirComunidade(comunidade);
+        Log log = new Log("Inserir", "Comunidade", "insert into comunidade (nome, criador, descricao, qnt_integrantes, foto) values ('" + nome + "', '" + criador + "', '" + descricao + "', " + qntIntegrantes + ", '" + foto + "')",
+                idAdm);
+        LogDAO logDAO = new LogDAO();
+        boolean logCerto = logDAO.inserirLog(log);
+
+        if (certo && logCerto) {
+            request.getSession().setAttribute("successMessage", "Categoria adicionada com sucesso!");
+            response.sendRedirect("jsp/categoria/adicionarCategoria.jsp");
+        } else {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao inserir categoria.");
+        }
+
+
 
     }
 }
